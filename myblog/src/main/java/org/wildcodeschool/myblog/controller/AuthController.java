eef1e993f705a0d7ade19efca71e3bab53274eba
Hélène Rampelberg <h.rampelberg@yahoo.fr>
2025-03-10 18:20:24 +0100

    Spring - Challenge 13 - User registration with JWT

--- myblog/src/main/java/org/wildcodeschool/myblog/controller/AuthController.java
+++ myblog/src/main/java/org/wildcodeschool/myblog/controller/AuthController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.wildcodeschool.myblog.Service.AuthenticationService;
 import org.wildcodeschool.myblog.Service.UserService;
+import org.wildcodeschool.myblog.dto.UserLoginDTO;
 import org.wildcodeschool.myblog.dto.UserRegistrationDTO;
 import org.wildcodeschool.myblog.model.User;
 
@@ -16,9 +18,11 @@ import java.util.Set;
 @RequestMapping("/auth")
 public class AuthController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
-    public AuthController(UserService userService) {
+    public AuthController(UserService userService,  AuthenticationService authenticationService) {
         this.userService = userService;
+        this.authenticationService = authenticationService;
     }
 
     @PostMapping("/register")
@@ -29,5 +33,14 @@ public class AuthController {
                 Set.of("ROLE_USER")
         );
         return ResponseEntity.status(HttpStatus.CREATED).body(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> authenticate(@RequestBody UserLoginDTO userLoginDTO) {
+        String token = authenticationService.authenticate(
+                userLoginDTO.getEmail(),
+                userLoginDTO.getPassword()
+        );
+        return ResponseEntity.ok("Token: " + token);
     }
 }
